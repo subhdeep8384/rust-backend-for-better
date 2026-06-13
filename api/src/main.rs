4c7f88f23c6ddfa66c07c15784e9417276e6afe2
Subhdeep pal <subhdeeppal7@gmail.com>
2026-06-13 11:39:01 +0530

    route made web

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,22 +1,31 @@
 use poem::{
-    Route, Server, get, handler, listener::TcpListener, post, web::Path
+    Route, Server, get, handler, listener::TcpListener, post, web::{Json, Path}
 };
 
+use crate::{request_inputs::CreateWebsiteInput, request_output::CreateWebsitesOutput};
+
+pub mod request_inputs;
+pub mod request_output;
+
 #[handler]
-async fn hello(Path(name): Path<String>) -> String {
-    format!("Hello {name}")
-}
-#[handler]
-async fn website(Path(website_id): Path<String>) -> String {
+async fn get_website(Path(website_id): Path<String>) -> String {
     format!("website : {}", website_id)
 }
 
-
+#[handler]
+async fn create_website(Json(data) : Json<CreateWebsiteInput>) -> Json<CreateWebsitesOutput> {
+    let url = data.url ;
+    // presist this in the database
+    let response = CreateWebsitesOutput{
+        id : String::from("ID")
+    } ;
+    Json(response) 
+}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = Route::new()
-    .at("/status/:website_id", get(website))
-    .at( "/website", post(hello))
+    .at("/website/:website_id", get(get_website))
+    .at( "/website", post(create_website))
     ;
 
     Server::new(TcpListener::bind("127.0.0.1:3000"))
